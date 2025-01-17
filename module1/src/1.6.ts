23ad2ade419e211075c2982f109e6042eb0ff8ad
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-01-17 17:03:57 +0600

    added type alice method

---
+++ module1/src/1.6.ts
@@ -0,0 +1,15 @@
+type Student = {
+  name: string;
+  age: number;
+  job: string;
+  contact?: string;
+  company: string;
+}
+
+const student: Student = {
+  name: 'sabbir',
+  age: 20,
+  job: 'web developer',
+  contact: 'dhaka',
+  company: 'nai',
+}
