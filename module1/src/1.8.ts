b3827c9399c604adf3b80768e45c48637ac4925a
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-01-19 11:25:59 +0600

    added concept type alias and interface

---
+++ module1/src/1.8.ts
@@ -0,0 +1,24 @@
+{
+  // Interface e  { er moddhe sob type likhe hoy }
+
+  // inter face e kono property add korte cayle extends name { er moddhe likhte hobe}
+
+  // type alias e - type name =  evabe  type likhte hoy
+
+  // type alias e barate hole type newName = ager kichu & { property: string }
+
+  type Roll = number[];
+
+  interface User {
+    name: string;
+    age: number;
+  }
+
+  const roll: Roll = [2, 3, 5, 76]
+
+  const user: User = {
+    name: 'sabbir',
+    age: 23,
+  }
+
+}
